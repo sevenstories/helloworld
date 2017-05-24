d9357392d4071ac3944fc2bc67a72d79e2746428
Joe Wheatley <joewheatley7@gmail.com>
2017-05-24 13:35:10 +0100

    added docker

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req, res){
-    res.send("hello world!, your ip is" + req.connection.remoteAddress);
+    res.send("welcome to the " + process.env.SERVICE_NAME +"!");
 });
 
 
